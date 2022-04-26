3b3ed8f2439981ccc4adff789bb705ff116feb29
Alberto <aalonsogarcia94@gmail.com>
2022-04-26 19:14:20 +0200

    BBDD Cambiada a Heroku

--- controllers/conection.js
+++ controllers/conection.js
@@ -1,10 +1,29 @@
 var mysql = require('mysql');
 
-var pool      =    mysql.createPool({
+var pool = mysql.createPool({
     connectionLimit : 10,
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
-    database : 'pleiaspace',
+    host     : 'eu-cdbr-west-02.cleardb.net',
+    user     : 'bb93f3ee1f045e',
+    password : '86aa4083',
+    database : 'heroku_1787a299a65ece9',
 });    
 module.exports = pool;
+
+
+// Pool LOCAL
+// var pool = mysql.createPool({
+//     connectionLimit : 10,
+//     host     : 'localhost',
+//     user     : 'root',
+//     password : '',
+//     database : 'pleiaspace',
+// });   
+
+// POOL HEROKU
+// var pool = mysql.createPool({
+//     connectionLimit : 10,
+//     host     : 'eu-cdbr-west-02.cleardb.net',
+//     user     : 'bb93f3ee1f045e',
+//     password : '86aa4083',
+//     database : 'heroku_1787a299a65ece9',
+// });    
