6e8d18463142660a80325c7ab2e805f42c901575
Alberto <aalonsogarcia94@gmail.com>
2022-05-02 14:40:17 +0200

    cokkiearreglo

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -6,11 +6,7 @@ function init(){
     if(document.cookie != ''){
         document.querySelector("#variables_perfil").innerHTML = `<a class="dropdown-item" href="/login">Perfil</a>
         <a class="dropdown-item" href="/api/logout">Logout</a>`
-    }else{
-        document.querySelector("#variables_perfil").innerHTML = `<a class="dropdown-item" href="/login">Login</a>
-        <a class="dropdown-item" href="/registro">Registro</a>`
     }
-
 }
 
 fetch("header.html")
