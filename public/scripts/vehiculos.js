0143ae36e973b4708b9c9c5ee47d461bf136be05
Alberto <aalonsogarcia94@gmail.com>
2022-06-01 21:28:59 +0200

    fix cabecera + vehicles schema

--- public/scripts/vehiculos.js
+++ public/scripts/vehiculos.js
@@ -5,11 +5,27 @@ function initVehicle(){
 }
 
 function animsVehicle(){
-    console.log(window.scrollY)
+    // console.log(window.scrollY)
 
-    if(window.scrollY >= 600){
-        document.querySelector("#titu1").style.position = 'relative'
-    }else{
-        document.querySelector("#titu1").style.position = 'sticky'
-    }
+    // let media_queryPEQUE = 'screen and (max-width:576px)';
+    // let media_queryMED = 'screen and (min-width:576px) and (max-width:1280px)';
+    // let dependiente = 0
+
+    // //Comprobar el mediaquery activo
+    // let coincidePeque = window.matchMedia(media_queryPEQUE).matches
+    // let coincideMed = window.matchMedia(media_queryMED).matches
+
+    // if(coincidePeque){
+    //     dependiente = 3000
+    // }else if(coincideMed){
+    //     dependiente = 3200
+    // }else{
+    //     dependiente = 2300
+    // }
+
+    // if(window.scrollY >= dependiente){
+    //     document.querySelector("#titu1").style.position = 'relative'
+    // }else{
+    //     document.querySelector("#titu1").style.position = 'sticky'
+    // }
 }
